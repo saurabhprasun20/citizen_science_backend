f48247464b98d4bbdad5e77153ea77940e1fb152
Prasun Saurabh <prasun.saurabh@uzh.ch>
2022-08-25 17:49:17 +0200

    Cors issue resolution

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/citizen-science")
 db = mongodb_client.db
 
--- src/backend/create_db_entry.py
+++ src/backend/create_db_entry.py
@@ -1,4 +1,4 @@
-import json
+import json,os
 from flask import Flask
 from flask_pymongo import PyMongo
 
@@ -6,16 +6,17 @@ app = Flask(__name__)
 mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/citizen-science")
 db = mongodb_client.db
 
-# json_file = open("src/backend/data/question_list.json")
-# question_data = json.load(json_file)
-# for i in range(0,50):
-#     db_list = []
-#     temp_list = question_data[str(i)]
-#     temp_list.sort()
-#     for question in temp_list:
-#         db_list.append("QID"+str(question))
+sibB = os.path.join(os.path.dirname(__file__), 'Flask-Server')
+json_file = open("src/backend/data/question_list.json")
+question_data = json.load(json_file)
+for i in range(0,50):
+    db_list = []
+    temp_list = question_data[str(i)]
+    temp_list.sort()
+    for question in temp_list:
+        db_list.append("QID"+str(question))
 
-#     db.question.insert_one({"question_list": db_list, "used":0})
+    db.question.insert_one({"question_list": db_list, "used":0})
 
 
 cursor = db.question.find_one({"used":0})
